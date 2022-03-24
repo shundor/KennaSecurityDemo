03f739e3290f3851a164bbc99a704a843aa526bf
Abir Majumdar <abirismyname@github.com>
2022-03-24 15:26:33 -0400

    Unbreaking

--- scripts/launch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bundle exec ruby toolkit.rb task=github_code_scanning github_token=$GITHUB_TOKEN kenna_connector_id=$KENNA_CONNECTOR_ID github_username=$GITHUB_USERNAME kenna_api_host=api.sandbox.us.kennasecurity.com kenna_api_key=$KENNA_API_KEY github_repositories=$GITHUB_REPOSITORIES
